7609d31bd708673d3020fbeef4128e3a4c860b53
coloradokim <schlesinger.kim@gmail.com>
2017-06-20 13:59:11 -0600

    add gif fadein

--- public/app.js
+++ public/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $('#play_audio').click(function() {
     $('audio').get(0).play();
+    setTimeout(function() {
+      $('.gif').fadeIn('slow');
+    }, 1500);
   });
 });
